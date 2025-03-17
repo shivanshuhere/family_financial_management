2752d05548dd776ab5b6437ffb29e8a52ea44128
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2025-03-17 16:00:36 +0000

    connect database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import app from "./app.js"
-
+import dbConnect from "./db/index.js"
 const PORT = 5000;
 
-app.listen(PORT,()=>{
-    console.log("Server is listening on the port "+ PORT);
+dbConnect().then(()=>{   
+    app.listen(PORT,()=>{
+        console.log("Server is listening on the port "+ PORT);
+    })
+}
+).catch((error)=>{
+    console.log("Failed to start sever ::  ", error);
+
 })
