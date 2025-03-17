f1405077d922283ddb34488ffeea7b6ca79ff897
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2025-03-17 15:59:14 +0000

    add middlewaer and user routes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,33 @@
-import express from "express"
-
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get("/",(req, res)=>{
 
+// middleware
+app.use(cors({
+    origin : "http://127.0.0.1:5173"
+}))
+
+app.use(json());
+app.use(urlencoded());
+app.use(cookieParser());
+dotenv.config({
+    path : "./.env"
+})
+
+// user routes
+import userRoutes from "./routes/user.routes.js";
+app.use("/api/auth", userRoutes);
+
+// test routes
+app.get("/",(req, res)=>{
     res.send("<h1>Home page</h1>")
 })
 
 app.get("/test",(req, res)=>{
-
     res.send("<h1>test page</h1>")
 })
 
