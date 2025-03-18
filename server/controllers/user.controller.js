1275e936db01588c396fb70423a1649127fa02ee
Shivanshu <ks7876555@gmail.com>
2025-03-19 00:23:32 +0530

    add user controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,43 @@
+import User from "../models/userModel.js";
+
+// Get All Users
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Update User Role
+export const updateUserRole = async (req, res) => {
+    const { role } = req.body;
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.role = role;
+            await user.save();
+            res.json({ message: "User role updated successfully" });
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Delete User
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            await user.deleteOne();
+            res.json({ message: "User deleted successfully" });
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
