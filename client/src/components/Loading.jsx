96c50a85ed98c1acbf15222deb53f56ad0477c52
Shivanshu <ks7876555@gmail.com>
2025-03-20 21:12:28 +0530

    add loader

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../style/Loader.css";
+function Loading() {
+    return (
+        <div className='w-full h-screen flex justify-center items-center text-black'>
+
+            <span className="loader"></span>
+        </div>
+
+    )
+}
+
+export default Loading
