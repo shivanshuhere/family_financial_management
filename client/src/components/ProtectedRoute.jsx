ec0fcda9e894b073aa51ef1f171d501e22649a7e
Shivanshu <ks7876555@gmail.com>
2025-03-20 21:16:27 +0530

    fix import

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context.jsx';
 
+
+
 const ProtectedRoute = ({ children, allowedRoles }) => {
     const { user } = useContext(AuthContext);
 
