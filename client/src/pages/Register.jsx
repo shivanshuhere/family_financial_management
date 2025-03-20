8371adcb8c6cde55deb47460ae5d39aa43b53e62
Shivanshu <ks7876555@gmail.com>
2025-03-20 21:49:02 +0530

    add loader and add error handling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,11 +3,12 @@ import { registerUser } from '../api/auth.js';
 import InputField from '../components/InputField';
 import Button from '../components/Button';
 import { useNavigate, Link } from 'react-router-dom';
-
+import Loading from '../components/Loading.jsx';
 const Register = () => {
     const navigate = useNavigate();
     const [formData, setFormData] = useState({ name: '', email: '', password: '' });
-
+    const [loading, setLoading] = useState(false)
+    const [msg, setMsg] = useState('')
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
 
@@ -15,45 +16,56 @@ const Register = () => {
 
     const handleRegister = async () => {
         try {
+            setLoading(true);
             const data = await registerUser(formData);
-            console.log("registerd user :: ", JSON.stringify(data));
+            setMsg(data?.response?.data?.message || "Registration Successful !")
+            console.log(data);
+
             navigate('/');
+
+
         } catch (error) {
-            alert(error.response?.data?.message || 'Register failed');
+            setMsg(error?.response?.data?.message || error?.message || "Registration Failed !")
+            console.error("error :: ", error)
+        }
+        finally {
+            setLoading(false)
         }
     };
 
     return (
-        <div className="h-screen flex justify-center items-center bg-gray-100">
-            <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-                <h2 className="text-2xl font-bold mb-6">Register</h2>
-                <InputField
-                    label="Name"
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-                <InputField
-                    label="Email"
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-                <InputField
-                    label="Password"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-                <Button text="Register" onClick={handleRegister} />
-                <div> Already have an account ?
-                    <Link to={'/register'} className='underline text-blue-600'> Login </Link>
+        loading ? <Loading /> :
+            <div className="h-screen flex justify-center items-center bg-gray-100">
+                <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+                    <h2 className="text-2xl font-bold mb-6">Register</h2>
+                    {msg && <p className="text-red-500 mb-4">{msg}</p>}
+                    <InputField
+                        label="Name"
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                    <InputField
+                        label="Email"
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <InputField
+                        label="Password"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <Button text="Register" onClick={handleRegister} />
+                    <div> Already have an account ?
+                        <Link to={'/'} className='underline text-blue-600'> Login </Link>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 };
 
