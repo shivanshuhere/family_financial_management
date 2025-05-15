2693ccd9c0f269382b794eca64dfb7f5546ecb17
Shivanshu <ks7876555@gmail.com>
2025-05-15 22:45:06 +0530

    add daizy ui

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -75,6 +75,12 @@ const Transactions = () => {
 
     return (
         <div className="container mx-auto p-8">
+            <div role="alert" className="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Transaction Added!</span>
+            </div>
             <h1 className="text-3xl font-bold mb-6">Manage Transactions</h1>
 
             {/* Add Transaction Form */}
