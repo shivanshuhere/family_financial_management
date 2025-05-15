1c67644d0b73f57dd2f4f47597ceff41ebcf0e96
Shivanshu <ks7876555@gmail.com>
2025-05-15 16:35:07 +0530

    remove sidebar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -8,9 +8,9 @@ const Layout = ({ children }) => {
 
     return (
         <div className="flex h-screen">
-            <Sidebar role={user?.role} />
+            {/* <Sidebar role={user?.role} /> */}
             <div className="flex-1 flex flex-col">
-                <Header />
+                <Header role={user?.role} />
                 <div className="p-6 bg-gray-100 flex-1 overflow-y-auto">{children}</div>
             </div>
         </div>
