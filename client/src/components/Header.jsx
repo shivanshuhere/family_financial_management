a49cf7a0c4ab8a89cbea14167a91b9e8c28a1922
Shivanshu <ks7876555@gmail.com>
2025-05-15 16:34:57 +0530

    style header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/auth.context.jsx';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ role }) => {
     const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = React.useState(false);
 
     const handleLogout = () => {
         logout();
@@ -12,10 +13,61 @@ const Header = () => {
     };
 
     return (
-        <div className="flex justify-between items-center bg-gray-700 p-4 text-white">
-            <h2 className="text-xl">Welcome, {user?.name}!</h2>
-            <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
-        </div>
+        // <div className="flex justify-between items-center bg-gray-700 p-4 text-white">
+        //     <h2 className="text-xl">Welcome, {user?.name}!</h2>
+        //     <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
+        // </div>
+
+        <nav className="relative bg-white shadow dark:bg-gray-800">
+            <div className="container px-6 py-4 mx-auto">
+                <div className="lg:flex lg:items-center lg:justify-between">
+                    <div className="flex items-center justify-between">
+                        <Link to="/dashboard">
+                            <img className="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
+                        </Link>
+
+                        <div className="flex lg:hidden">
+                            <button onClick={() => setIsOpen(!isOpen)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400 lg:hidden" aria-label="toggle menu"
+
+                            >
+                                {!isOpen ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+                                    </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full lg:translate-x-0 lg:opacity-100'}`}>
+                        <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
+                            <Link to="/dashboard" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0" >Dashboard</Link>
+                            {role === 'Admin' && (<>
+                                <Link to="/manage-users" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0">Manage Users</Link>
+                            </>)}
+                            {(role === 'Family Member' || role === 'Accountant') && (<>
+                                <Link to="/reports" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0">Reports</Link>
+                                <Link to="/transactions" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0">Transactions</Link>
+                                <Link to="/insights" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0">Ai Insights</Link></>)}
+                        </div>
+
+                        <div className="flex items-center mt-4 lg:mt-0">
+                            <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                                <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
+                                    <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" className="object-cover w-full h-full" alt="avatar" />
+                                </div>
+
+                                <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">{user?.name || 'Guest'}</h3>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
