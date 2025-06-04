50dd6ee235fcaf361433369501c5360487ece755
Shivanshu <ks7876555@gmail.com>
2025-06-04 18:50:51 +0530

    fix text color

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
                                 <form>
                                     <div className="w-full mt-4">
                                         <input
-                                            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                                            className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                                             type="email"
                                             placeholder="Email Address"
                                             aria-label="Email Address"
@@ -89,7 +89,7 @@ const Login = () => {
 
                                     <div className="w-full mt-4">
                                         <input
-                                            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password"
+                                            className="block w-full px-4 py-2 mt-2 text-white placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password"
                                             placeholder="Password"
                                             aria-label="Password"
                                             value={password}
